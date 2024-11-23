54df47ef2c4b08f4db7fa4779ac3b9a0392acea1
ethar615 <ethar.almaghrabi@ontariotechu.net>
2024-11-22 22:07:17 -0500

    db cloud and comments

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 *Github Repository:* https://github.com/ethar615/RecipeManagerTracker.git 
 CSS used by this and edited by me: https://startbootstrap.com/theme/business-casual#google_vignette
 Confirm deletion button: https://www.w3schools.com/js/js_popup.asp 
+Link to website: 
 
 Notes: 
 1. To run this program you need to create a mongoDB and collection in your local machine
--- server/config/db.js
+++ server/config/db.js
@@ -3,4 +3,10 @@
 //MongoDB details:
 //Database Name: RecipeLib
 //Collection name: Cooking_recipes
-module.exports={"URI":"mongodb://localhost:27017/RecipeLib"}
+
+//Local DB (commented)
+//module.exports={"URI":"mongodb://localhost:27017/RecipeLib"}
+
+
+// Cloud DB at Mangodb.com
+module.exports={"URI":"mongodb+srv://etharalmaghrabi:7se3PYuqsYqlCQPy@cluster0.g2mfj.mongodb.net/RecipeLib"}
